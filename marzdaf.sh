a6a881753a0bfab6e0a4fb925c4a2c4487995111
daffahelmi <39628395+daffahelmi@users.noreply.github.com>
2024-02-24 15:07:28 +0700

    Create marzdaf.sh

---
+++ marzdaf.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+sfile="https://github.com/daffahelmi/marzdaf/blob/main"
+
+#domain
+read -rp "Masukkan Domain: " domain
+echo "$domain" > /root/domain
+domain=$(cat /root/domain)
+
+#email
+read -rp "Masukkan Email anda: " email
+
+#Preparation
+clear
+cd;
+apt-get update;
+
+#Remove unused Module
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove sendmail*;
+apt-get -y --purge remove bind9*;
+
+#install bbr
+echo 'fs.file-max = 500000
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 4096
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mem = 25600 51200 102400
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.core.rmem_max=4000000
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.ip_forward=1
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+net.ipv6.conf.all.disable_ipv6=1
+net.ipv6.conf.default.disable_ipv6=1
+net.ipv6.conf.lo.disable_ipv6=1' >> /etc/sysctl.conf
+sysctl -p;
+
