e403048e77b6211f933858bc48b630f699415e17
daffahelmi <39628395+daffahelmi@users.noreply.github.com>
2024-10-28 00:13:23 +0700

    Update and rename haproxy.cfg.template to haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
+
+# Baca domain dari file
+domain=$(cat /root/domain)
+
+# Hasilkan konfigurasi HAProxy dengan domain
+cat <<EOF > /etc/haproxy/haproxy.cfg
 listen front
     mode tcp
     bind *:443
     tcp-request inspect-delay 5s
     tcp-request content accept if { req_ssl_hello_type 1 }
     use_backend fallback if { req.ssl_sni -i end $domain }
-backend fallback
 
+backend fallback
     mode tcp
     server srv1 127.0.0.1:11000
+EOF
